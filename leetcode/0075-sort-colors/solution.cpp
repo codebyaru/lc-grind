264a1ae522cd250a4a69b520d06d953c2c53a8b2
Arushi Jain <arushij0607@gmail.com>
2025-09-28 08:26:52 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.6 MB (47.90%)

--- leetcode/0075-sort-colors/solution.cpp
+++ leetcode/0075-sort-colors/solution.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int low=0;
-        int mid = 0;
-        int high = nums.size()-1;
-        while (mid<=high)
+        int low=0,mid=0,high=nums.size()-1;
+        while (mid<high)
         {
-            if (nums[mid]==0)
+            for (int i=0; i<nums.size();i++)
             {
-            swap(nums[low],nums[mid]);
-            low ++ , mid++;
-            }
-            else if (nums[mid]==1)
-            mid ++;
-            else if (nums[mid]==2)
-            {
-                swap(nums[high],nums[mid]);
-                high-- ;
-            }
+                if (nums[mid]==0)
+                {
+                    swap(nums[low],nums[mid]);
+                    low++,mid++;
+                }
+                else if (nums[mid]==1)
+                {
+                    mid++;
+                }
+                else if (nums[mid]==2)
+                {
+                    swap(nums[high],nums[mid]);//high ki jgah 2 aa jayega
+                    high--;
+                }
         }
+        return;
+    }
     }
 };
