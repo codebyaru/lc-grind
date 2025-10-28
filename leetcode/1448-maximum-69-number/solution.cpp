f27abf2d247680e4f0604ac8b39654474e8d035c
Arushi Jain <arushij0607@gmail.com>
2025-10-28 15:38:51 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8 MB (69.52%)

--- leetcode/1448-maximum-69-number/solution.cpp
+++ leetcode/1448-maximum-69-number/solution.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
     int maximum69Number (int num) {
-        vector<int> digits;
+        vector <int> digits;
         int n = num;
-
-        // extract digits (reverse order)
-        while(n > 0) {
-            digits.push_back(n % 10);
-            n /= 10;
-        }
-
-        // reverse to correct order
+        while (n>0)
+        {
+            digits.push_back(n%10);
+            n=n/10;
+        } 
         reverse(digits.begin(), digits.end());
 
-        // change first 6 to 9
-        for(int i = 0; i < digits.size(); i++) {
-            if(digits[i] == 6) {
-                digits[i] = 9;
-                break;
+        for (int i=0; i<digits.size();i++)
+        {
+            if (digits[i]==6)
+            {
+                digits[i]=9;      break;
             }
+        } 
+        int ans =0;
+        for (int i=0; i<digits.size();i++)
+        {
+            ans = ans*10+digits[i];
         }
-
-        // convert back to integer
-        int ans = 0;
-        for(int i = 0; i < digits.size(); i++)
-            ans = ans * 10 + digits[i];
-
-        return ans;
+        return ans;   
     }
 };
-
