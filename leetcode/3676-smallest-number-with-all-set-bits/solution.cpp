9a6364b0ea157e0969c2f6b279c994d28a7bae88
Arushi Jain <arushij0607@gmail.com>
2025-10-29 15:03:47 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.3 MB (24.91%)

---
+++ leetcode/3676-smallest-number-with-all-set-bits/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int smallestNumber(int n) {
+       for (int i=0;i<=n;i++)
+       {
+        if (pow(2,i)>n)
+        return (pow(2,i)-1);
+       } 
+       return 0;
+    }
+};
