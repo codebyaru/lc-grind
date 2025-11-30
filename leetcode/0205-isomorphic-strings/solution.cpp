c62c68969a824e950f42f159461d9760d95b116b
Arushi Jain <arushij0607@gmail.com>
2025-11-30 05:59:04 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.5 MB (7.15%)

--- leetcode/0205-isomorphic-strings/solution.cpp
+++ leetcode/0205-isomorphic-strings/solution.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-    if (s.size() != t.size()) return false;
-
-    int mapS[256] = {0}, mapT[256] = {0};
-
-    for (int i = 0; i < s.size(); i++) {
-        if (mapS[s[i]] != mapT[t[i]]) return false;
-
-        // +1 because 0 means “not set” yet
-        mapS[s[i]] = i + 1;
-        mapT[t[i]] = i + 1;
+        if (s.size() != t.size())
+            return false;
+        // compare the indices of the character
+        // mpp1 check karta hai ki s ka har character sirf ek fixed character t
+        // me map ho.
+        // mpp2 ensure karta hai ki t ka koi character do-do characters se map
+        // na ho.
+        unordered_map<char, char> mpp1;
+        unordered_map<char, bool> mpp2;
+        for (int i = 0; i < s.length(); i++) {
+            if (mpp1.count(s[i])) {
+                if (mpp1[s[i]] != t[i])
+                    return false;
+            } else {
+                if (mpp2[t[i]])
+                    return false;
+                mpp1[s[i]] = t[i];
+                mpp2[t[i]] = true;
+            }
+        }
+        return true;
     }
-
-    return true;
-}
-
 };
