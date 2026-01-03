82abc9eb3fdcab3c509b0ae787e9baf520f4ae4f
Arushi Jain <arushij0607@gmail.com>
2026-01-03 14:48:20 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.9 MB (52.25%)

---
+++ leetcode/4177-reverse-string-prefix/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string reversePrefix(string s, int k) {
+        int l=0; int r = k-1;
+        while(l<r){
+            swap(s[l],s[r]);
+            l++; r--;
+        }
+        return s;
+    }
+};
