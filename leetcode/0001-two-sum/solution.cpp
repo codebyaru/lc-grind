f447325e1b255d4ab16ce29ce470eac817ad3ac5
Arushi Jain <arushij0607@gmail.com>
2026-01-15 07:52:35 +0000

    Sync LeetCode submission Runtime - 2 ms (71.89%), Memory - 15.9 MB (7.22%)

--- leetcode/0001-two-sum/solution.cpp
+++ leetcode/0001-two-sum/solution.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> sum;
-       
-            for (int i = 0; i < nums.size(); i++) {
-                for (int j = i+1; j < nums.size(); j++) {
-                    if (nums[i] + nums[j] == target) {
-                        sum.push_back(i);
-                        sum.push_back(j);
-                      return sum;
-                    }
-                }
-            }
+        unordered_map<int,int> mp;
 
-        return sum;
+        // pass 1: store value → index
+        for(int i = 0; i < nums.size(); i++)
+            mp[nums[i]] = i;
+
+        // pass 2: check partner
+        for(int i = 0; i < nums.size(); i++){
+            int need = target - nums[i];
+            if(mp.count(need) && mp[need] != i)
+                return {i, mp[need]};
+        }
+        return {};
     }
 };
+
