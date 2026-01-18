7796ad763ddc286db31ef07cc148393b4192e267
Arushi Jain <arushij0607@gmail.com>
2026-01-18 11:38:37 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 28.2 MB (63.45%)

--- leetcode/0169-majority-element/solution.cpp
+++ leetcode/0169-majority-element/solution.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int n=nums.size();
-       map <int,int>mp;
-       for (int i=0; i<n;i++)
-       {
-        mp[nums[i]]++;
-       }
-       for (auto it: mp)
-       {
-        if (it.second >n/2)
-        return it.first;
-       }
-return 0;
+        int cnt = 0;
+        int candidate = 0;
+        for (int x : nums) {
+            if (cnt == 0)
+                candidate = x;
+            if (x == candidate)
+                cnt++;
+            else
+                cnt--;
+        }
+        return candidate;
     }
 };
