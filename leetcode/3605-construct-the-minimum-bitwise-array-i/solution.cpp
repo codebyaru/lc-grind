2cc0b71b2201a69f8ef6df5a39e96d738f6c7b82
Arushi Jain <arushij0607@gmail.com>
2026-01-20 16:38:33 +0000

    Sync LeetCode submission Runtime - 1 ms (43.85%), Memory - 25.1 MB (60.09%)

---
+++ leetcode/3605-construct-the-minimum-bitwise-array-i/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> minBitwiseArray(vector<int>& nums) {
+        for (int i=0;i<nums.size();i++){
+            int temp=nums[i];
+            for (int j=1; j<nums[i];j++){
+                if((j|j+1)==nums[i]) {
+                    nums[i]=j; break;
+                }
+            }
+            if(nums[i]==temp) nums[i]=-1;
+        }
+        return nums;
+    }
+};
