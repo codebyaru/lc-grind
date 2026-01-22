6e1ee6d529554e21054f2ea8245f18396cff80c1
Arushi Jain <arushij0607@gmail.com>
2026-01-22 08:03:21 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.2 MB (21.40%)

--- leetcode/0008-string-to-integer-atoi/solution.cpp
+++ leetcode/0008-string-to-integer-atoi/solution.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-    int myAtoi(string s) {
-        int i = 0;
-        int sign = 1;
-        long result = 0;
-        while (i < s.size() && s[i] == ' ')
-            i++;
-        if (i == s.size())
-            return 0;
-        if (s[i] == '-') {
-            sign = -1;
-            i++;
-        } else if (s[i] == '+')
-            i++;
+int helper(string&s, int i, long long num, int sign){
+    if(i>=s.size()|| !isdigit(s[i]))
+    return sign*num;
+    num = num*10+(s[i]-'0');
 
-        // converting chars to string
-        while (i<s.size() && isdigit(s[i]))
-        {
-            result = result*10+ (s[i]-'0');
-            if(sign*result > INT_MAX) return INT_MAX;
-            else if (sign*result < INT_MIN) return INT_MIN;
-            i++;
+    if(sign*num<=INT_MIN) return INT_MIN;
+    if(sign*num>=INT_MAX) return INT_MAX;
+
+    return helper(s, i+1, num, sign);
+}
+    int myAtoi(string s) {
+        int i=0; 
+        while(i<s.size() && s[i] ==' ') i++;
+        int sign =1;
+        if(i<s.size()&&(s[i]=='+' || s[i] =='-')){
+            if(s[i]=='-') sign = -1; i++;
         }
-        return (int)(sign*result);
+        return helper(s,i,0,sign);
     }
 };
