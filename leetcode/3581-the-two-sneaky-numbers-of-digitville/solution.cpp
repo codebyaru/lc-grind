6066c4124c1f235fe24311b2e19f1ffeca42a543
Arushi Jain <arushij0607@gmail.com>
2026-02-13 14:51:40 +0000

    Sync LeetCode submission Runtime - 4 ms (22.65%), Memory - 26.4 MB (20.75%)

--- leetcode/3581-the-two-sneaky-numbers-of-digitville/solution.cpp
+++ leetcode/3581-the-two-sneaky-numbers-of-digitville/solution.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     vector<int> getSneakyNumbers(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        vector<int> ans;
-
-        for (int i = 1; i < nums.size(); i++) {
-            if (nums[i] == nums[i - 1])
-                ans.push_back(nums[i]);
-        }
+        set<int>seen; vector<int>ans;
+        for(int x:nums){
+            if(seen.count(x)) ans.push_back(x);
+            else seen.insert(x);}
         return ans;
     }
 };
-
