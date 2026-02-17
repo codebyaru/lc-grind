f9347384dc5d88f9378ed8f0ac2c29d2556bf929
Arushi Jain <arushij0607@gmail.com>
2026-02-17 16:35:02 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.7 MB (43.52%)

--- leetcode/2137-final-value-of-variable-after-performing-operations/solution.cpp
+++ leetcode/2137-final-value-of-variable-after-performing-operations/solution.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     int finalValueAfterOperations(vector<string>& operations) {
-        int temp = 0;
-        int x = 0;
+        int n = 0;
         for (int i = 0; i < operations.size(); i++) {
-            if (operations[i].find('+') != string::npos) {
-                x = x + 1;
-            } else
-                x = x - 1;
+            if (operations[i][1] == '+')
+                n++;
+            else n--;
         }
-        temp = x;
-        return temp;
+        return n;
     }
 };
