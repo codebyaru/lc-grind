9e2a14a69755b8e842ae362dbe6205f04aacfe1b
Arushi Jain <arushij0607@gmail.com>
2026-02-17 01:40:07 +0000

    Sync LeetCode submission Runtime - 7 ms (19.14%), Memory - 9.7 MB (47.76%)

--- leetcode/0242-valid-anagram/solution.cpp
+++ leetcode/0242-valid-anagram/solution.cpp
@@ -1,16 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if (s.size()!=t.size())
-        return false;
-        for (int i=0; i<s.size();i++)
-        {
-            if(s[i]!=t[i])
-            return false;   
-        }
-        return true;
-
+        sort(s.begin(), s.end());
+        return s == (sort(t.begin(), t.end()), t);
     }
 };
+
