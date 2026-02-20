acdaa0c0d24fe99f97ab3d82ff4932cd5b79e8b5
Arushi Jain <arushij0607@gmail.com>
2026-02-20 18:54:03 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13 MB (50.89%)

---
+++ leetcode/0696-count-binary-substrings/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countBinarySubstrings(string s) {
+       int prev=0, curr=1,  ans=0; 
+       for (int i=1;i<s.size();i++){
+        if(s[i]!=s[i-1]){
+            ans+=min(prev,curr); //selecting the min group el
+            prev = curr;
+            curr=1;
+        }
+        else 
+        curr++;
+       }
+    return ans + min(prev,curr);
+    }
+};
