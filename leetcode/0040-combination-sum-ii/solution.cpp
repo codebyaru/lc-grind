d5c93657956e42d09bc167f8eb768cfc6904e4be
Arushi Jain <arushij0607@gmail.com>
2026-02-28 08:12:44 +0000

    Sync LeetCode submission Runtime - 6 ms (11.21%), Memory - 15.9 MB (10.44%)

--- leetcode/0040-combination-sum-ii/solution.cpp
+++ leetcode/0040-combination-sum-ii/solution.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
-void findcombi(int ind, int target, vector<int>&arr, vector<vector<int>>&ans, vector<int>ds){
-    if(target==0){
-        ans.push_back(ds);
-        return;}
-//i=ind is used to prevent reusing prev elemenst 
-    for(int i=ind;i<arr.size();i++){
-        if(i>ind && arr[i]==arr[i-1]) continue;
-        if(arr[i]>target) break;
-        ds.push_back(arr[i]);
-        findcombi(i+1, target-arr[i], arr, ans, ds);
-        ds.pop_back();
+    void solve(int ind, int target, vector<int>& arr,
+               vector<int>& ds, vector<vector<int>>& ans) {
+        if(target == 0){
+            ans.push_back(ds);
+            return;
+        }
+        unordered_set<int> used;   // prevent duplicates at this level
+        for(int i = ind; i < arr.size(); i++){
+            if(arr[i] > target) continue;   // optional prune
+            if(used.count(arr[i])) continue; // skip duplicate branch
+            used.insert(arr[i]);
+            ds.push_back(arr[i]);
+            solve(i+1, target-arr[i], arr, ds, ans);
+            ds.pop_back();
+        }
     }
-}
+
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(),candidates.end());
-        vector<int>ds;
-        vector<vector<int>>ans;
-        findcombi(0, target, candidates, ans, ds);
+        sort(candidates.begin(), candidates.end()); 
+        vector<vector<int>> ans;
+        vector<int> ds;
+        solve(0, target, candidates, ds, ans);
         return ans;
     }
 };
