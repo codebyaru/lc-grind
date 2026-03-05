ef5323c04ebc9b3dcc59bb48d0a4bc5746fc46ff
Arushi Jain <arushij0607@gmail.com>
2026-03-05 10:21:13 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.6 MB (19.00%)

--- leetcode/0034-find-first-and-last-position-of-element-in-sorted-array/solution.cpp
+++ leetcode/0034-find-first-and-last-position-of-element-in-sorted-array/solution.cpp
@@ -1,40 +1,16 @@
 class Solution {
 public:
-    int firstocc(vector<int>&nums, int target)
-    {
-        int low=0; int high = nums.size()-1; int ans1=-1;
-        while (low<=high)
-        {
-            int mid = (low+high)/2;
-            if (nums[mid]==target)
-            {
-                ans1=mid;
-                high = mid-1;
-            }
-            else if (nums[mid]<target)
-            low = mid+1;
-            else 
-            high = mid-1;
-        }
-        return ans1;
-    }
-    int lastocc(vector<int>&nums, int target)
-    {
-    int low=0; int high = nums.size()-1; int ans1=-1;
-        while (low<=high)
-        {
-            int mid = (low+high)/2;
-            if (nums[mid]==target)
-            { ans1=mid; low = mid+1;}
-            else if (nums[mid]<target) low = mid+1;
-            else 
-            high = mid-1;
-        }
-        return ans1;
-    }
     vector<int> searchRange(vector<int>& nums, int target) {
-        int first = firstocc(nums,target);
-        int last = lastocc(nums,target);
-        return {first,last};
+        int start = -1, end = -1;
+
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] == target){
+                if(start == -1)
+                    start = i;
+                end = i;
+            }
+        }
+
+        return {start, end};
     }
 };
