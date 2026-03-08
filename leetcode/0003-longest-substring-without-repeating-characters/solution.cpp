8807595175a74427269a0680a1d5913adf67583a
Arushi Jain <arushij0607@gmail.com>
2026-03-08 12:47:49 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.4 MB (85.96%)

--- leetcode/0003-longest-substring-without-repeating-characters/solution.cpp
+++ leetcode/0003-longest-substring-without-repeating-characters/solution.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_set<char>c;
-        int ans = 0; int i=0;
-        for (int j=0;j<s.size();j++)
-        {
-            while (c.count(s[j]))
-            {
-                c.erase(s[i]);
-                i++;
-            }
-            c.insert(s[j]);
-            ans = max(ans, j-i+1);
+     int n=s.size();
+     int hash[256];
+     for (int i=0;i<256;i++){
+        hash[i]=-1;
+     }   
+
+     int l=0, r=0, maxi=0;
+     while(r<n){
+        if(hash[s[r]]!=-1){
+            if(hash[s[r]]>=l)
+            l=hash[s[r]]+1;
         }
-        return ans;
+        int len = r-l+1;
+        maxi=max(maxi, len);
+        hash[s[r]]=r;
+        r++;
+     }
+     return maxi;
     }
 };
