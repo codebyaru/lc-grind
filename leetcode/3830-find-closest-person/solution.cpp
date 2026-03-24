b8293d26c2eb32776517289e375784fcb713d4bd
Arushi Jain <arushij0607@gmail.com>
2026-03-24 18:43:41 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.6 MB (20.54%)

--- leetcode/3830-find-closest-person/solution.cpp
+++ leetcode/3830-find-closest-person/solution.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
     int findClosest(int x, int y, int z) {
-        int step1 = abs(z - x);
-        int step2 = abs(y - z);
-        if (step1 < step2)
-            return 1;
-        else if (step1 > step2)
-            return 2;
-        else
-            return 0;
+       int d1=abs(z-x); int d2=abs(z-y);
+       if(d1>d2) return 2;
+       else if(d1<d2) return 1; 
+       return 0; 
     }
 };
