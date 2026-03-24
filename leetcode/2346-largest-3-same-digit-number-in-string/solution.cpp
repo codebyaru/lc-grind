f5262774112311703b1fd9b92cd4c6e8e4b08b52
Arushi Jain <arushij0607@gmail.com>
2026-03-24 13:33:16 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9 MB (78.91%)

--- leetcode/2346-largest-3-same-digit-number-in-string/solution.cpp
+++ leetcode/2346-largest-3-same-digit-number-in-string/solution.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
     string largestGoodInteger(string num) {
-    char maxDigit = 0; // to store the biggest digit found
-    int count = 1;
-
-    for(int i = 1; i < num.size(); i++) {
-        if(num[i] == num[i-1]) {
-            count++;
-            if(count == 3) { // found 3 same digits
-                maxDigit = max(maxDigit, num[i]);
+        string ans ="";
+        for (int i=0;i<num.size()-2;i++){
+            if(num[i]==num[i+1] && num[i]==num[i+2]){
+                string temp = num.substr(i,3);
+                if(temp>ans) ans = temp;
             }
-        } else {
-            count = 1; // reset counter
-        }
-    }
-
-    if(maxDigit == 0) return ""; // no good integer found
-    return string(3, maxDigit);
+        } 
+        return ans;
     }
 };
