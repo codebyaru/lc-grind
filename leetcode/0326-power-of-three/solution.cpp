9576b2fece5663736b5eed541938671deb358b0b
Arushi Jain <arushij0607@gmail.com>
2026-03-24 13:37:44 +0000

    Sync LeetCode submission Runtime - 5 ms (30.18%), Memory - 9 MB (42.66%)

--- leetcode/0326-power-of-three/solution.cpp
+++ leetcode/0326-power-of-three/solution.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        while (n <= 0)
-            return false;
-        while (n % 3 == 0) {
-            n = n / 3;
+        if(n<1)
+        return 0;
+        while(n!=1){
+            if(n%3!=0)
+            return 0;
+            n/=3;
         }
-        return n==1;
+        return 1;
     }
 };
