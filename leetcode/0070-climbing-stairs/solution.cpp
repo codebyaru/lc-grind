9aac7908ea087c01b1c6b4169f638d3d31d55930
Arushi Jain <arushij0607@gmail.com>
2026-03-24 16:53:52 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.8 MB (85.51%)

--- leetcode/0070-climbing-stairs/solution.cpp
+++ leetcode/0070-climbing-stairs/solution.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int>dp(n+1,-1);
-        dp[0]=1;
-        dp[1]=1;
-        for (int i=2;i<dp.size();i++)
-        {
-            dp[i] = dp[i-1]+dp[i-2];
-           // n=n+dp[i];
+        if (n == 0 || n == 1) {
+            return 1;
         }
-        return dp[n];     
+        int prev = 1, curr = 1;
+        for (int i = 2; i <= n; i++) {
+            int temp = curr;
+            curr = prev + curr;
+            prev = temp;
+        }
+        return curr;
     }
 };
