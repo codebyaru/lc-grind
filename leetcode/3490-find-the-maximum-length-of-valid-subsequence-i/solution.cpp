2fbd355ee3d71f1a946975e3ab31ba7b03e1fb7d
Arushi Jain <arushij0607@gmail.com>
2026-03-25 13:24:04 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 96.2 MB (66.67%)

--- leetcode/3490-find-the-maximum-length-of-valid-subsequence-i/solution.cpp
+++ leetcode/3490-find-the-maximum-length-of-valid-subsequence-i/solution.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
     int maximumLength(vector<int>& nums) {
-        int oo=0, ee=0, oe=1;
-        int prev = nums[0]%2;
-        if (prev)
-        oo++;
-        else ee++;
-        for (int i=1; i<nums.size(); i++)
+        int e=0,o=0, alternating =0; int lastparity = -1;
+        for (int i:nums){
+            int parity = i%2;
+            if(parity==0) e++;
+            else o++;
+        if(parity!=lastparity)
         {
-            int parity = nums[i]%2;
-            if (parity ==0) ee++;
-            else oo++;
-            if (prev !=parity)
-            {
-                oe++;
-                prev = parity;
-            }
-        } 
-     return max(max(oo,oe),ee);   
+            alternating ++;
+            lastparity = parity;
+        }
+        }
+        return max({o,e,alternating});
     }
 };
