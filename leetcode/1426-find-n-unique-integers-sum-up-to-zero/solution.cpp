4f2ff7b53fefe327cda2a36d3cc0da8e64257a40
Arushi Jain <arushij0607@gmail.com>
2026-03-26 06:40:49 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.7 MB (16.42%)

--- leetcode/1426-find-n-unique-integers-sum-up-to-zero/solution.cpp
+++ leetcode/1426-find-n-unique-integers-sum-up-to-zero/solution.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     vector<int> sumZero(int n) {
-        vector<int> v;
-        for (int i = -n / 2; i <= n / 2; i++) {
-            v.push_back(i);
+        vector<int>ans;
+        if(n%2) ans.push_back(0);
+        for (int i=1;i<=n/2;i++){
+            ans.push_back(i);
+            ans.push_back(-i);
         }
-        if (n % 2 == 0)
-            v.erase(v.begin() + n / 2);
-        return v;
+        return ans;
     }
 };
