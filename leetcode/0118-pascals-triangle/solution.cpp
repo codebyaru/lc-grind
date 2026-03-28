db02e587c26ae6a7a5e3b43721719f122f81403c
Arushi Jain <arushij0607@gmail.com>
2026-03-28 12:40:14 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.7 MB (62.41%)

--- leetcode/0118-pascals-triangle/solution.cpp
+++ leetcode/0118-pascals-triangle/solution.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> ans = {{1}};
-        for (int i = 1; i < numRows; i++) {
-            vector<int> temp;
-            for (int j = 0; j <= i; j++) {
-                if (j == 0 || j == i)
-                    temp.push_back(1);
-                else
-                    temp.push_back(ans[i - 1][j - 1] + ans[i - 1][j]);
+        vector<vector<int>>ans;
+        for (int i=0;i<numRows;i++){
+            vector<int>row(i+1,1);
+            for(int j=1;j<i;j++){
+                row[j]=ans[i-1][j-1]+ans[i-1][j];
             }
-            ans.push_back(temp);
+            ans.push_back(row);
         }
         return ans;
     }
