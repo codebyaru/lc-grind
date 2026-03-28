b2caf8e66a9e479e3110181dc32dcd81bac69cf5
Arushi Jain <arushij0607@gmail.com>
2026-03-28 19:20:33 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8 MB (39.50%)

--- leetcode/1448-maximum-69-number/solution.cpp
+++ leetcode/1448-maximum-69-number/solution.cpp
@@ -1,27 +1,13 @@
 class Solution {
 public:
     int maximum69Number (int num) {
-        vector <int> digits;
-        int n = num;
-        while (n>0)
-        {
-            digits.push_back(n%10);
-            n=n/10;
-        } 
-        reverse(digits.begin(), digits.end());
-
-        for (int i=0; i<digits.size();i++)
-        {
-            if (digits[i]==6)
-            {
-                digits[i]=9;      break;
-            }
-        } 
-        int ans =0;
-        for (int i=0; i<digits.size();i++)
-        {
-            ans = ans*10+digits[i];
+        string s = to_string(num);
+        for (char &i:s){
+            if(i=='6') {
+                i='9';
+                break;
         }
-        return ans;   
+        }
+        return stoi(s);
     }
 };
