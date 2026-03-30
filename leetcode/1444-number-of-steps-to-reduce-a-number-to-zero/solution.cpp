2e52bff7f57acbc2c2064b471eef28dbf283c8df
Arushi Jain <arushij0607@gmail.com>
2026-03-30 12:47:22 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.9 MB (79.16%)

---
+++ leetcode/1444-number-of-steps-to-reduce-a-number-to-zero/solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int numberOfSteps(int num) {
+        int c=0;
+        while(num>0){
+        if(num%2==0) {num=num/2;c++;}
+         if(num%2!=0) {num=num-1;c++;}}
+        return c;
+    }
+};
