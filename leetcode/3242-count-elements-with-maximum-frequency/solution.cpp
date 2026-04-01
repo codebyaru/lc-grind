e2885911fd2325670726327274440cf1fb1ce0b0
Arushi Jain <arushij0607@gmail.com>
2026-04-01 05:22:18 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 23.2 MB (67.64%)

--- leetcode/3242-count-elements-with-maximum-frequency/solution.cpp
+++ leetcode/3242-count-elements-with-maximum-frequency/solution.cpp
@@ -1,27 +1,16 @@
 class Solution {
 public:
     int maxFrequencyElements(vector<int>& nums) {
-        vector<int> v1(nums.size(), 1); // initially freq = 1 for each element
-
-        // count frequencies
-        for (int i = 0; i < nums.size(); i++) {
-            for (int j = i + 1; j < nums.size(); j++) {
-                if (nums[i] == nums[j]) {
-                    v1[i]++;   // count at position i
-                }
-            }
+        unordered_map<int,int> freq;   
+        for (int x : nums) freq[x]++;
+        int maxi = 0;
+        for (auto it : freq) {
+            maxi = max(maxi, it.second);
         }
-
-        // find max freq
-        int maxf = *max_element(v1.begin(), v1.end());
-
-        // sum all elements with max freq
-        int ans = 0;
-        for (int x : v1) {
-            if (x == maxf) ans += x;
+        int count = 0;
+        for (auto it : freq) {
+            if (it.second == maxi) count += maxi;
         }
-
-        return ans;
+        return count;
     }
 };
-
