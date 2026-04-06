649df07a33b9f133fbe1405dbb8718ae837c7940
Arushi Jain <arushij0607@gmail.com>
2026-04-06 14:20:47 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 20.7 MB (60.44%)

--- leetcode/0073-set-matrix-zeroes/solution.cpp
+++ leetcode/0073-set-matrix-zeroes/solution.cpp
@@ -1,30 +1,20 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
+       int row=matrix.size(); int col=matrix[0].size();
+       vector<int>r(row,0),c(col,0);
+       for(int i=0;i<row;i++){
+        for (int j=0;j<col;j++){
+            if(matrix[i][j]==0) {r[i]=1; c[j]=1;}
 
-        vector<int> row(m, 0);
-        vector<int> col(n, 0);
-
-        // mark rows and cols
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == 0) {
-                    row[i] = 1;
-                    col[j] = 1;
-                }
-            }
         }
+       }
 
-        // set zeroes
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (row[i] || col[j]) {
-                    matrix[i][j] = 0;
-                }
-            }
+       for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                if(r[i]==1 || c[j]==1)  matrix[i][j]=0;
         }
+       }
+       return ;
     }
 };
-
