bcf97af5bfa9d72784e406850e658e1d17ae93ae
Arushi Jain <arushij0607@gmail.com>
2026-04-09 17:53:41 +0000

    Sync LeetCode submission Runtime - 125 ms (96.39%), Memory - 29.7 MB (22.87%)

---
+++ leetcode/0204-count-primes/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+       if(n<=2) return 0;
+       int prime[n];
+       for (int i=0;i<n;i++) prime[i]=1;
+       prime[0]=prime[1]=0;
+       for (int i=2;i*i<n;i++){
+        if(prime[i]==1) {
+            for (int j=i*i;j<n;j+=i){
+                prime[j]=0;
+            }
+        }
+       }
+       int c=0; 
+       for (int i=2;i<n;i++) {
+        if(prime[i]==1) c++;
+       }
+       return c;
+    }
+};
