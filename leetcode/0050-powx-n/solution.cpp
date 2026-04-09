ab1288a5fc7bdf98404f8dfa762a8ebcb517ca44
Arushi Jain <arushij0607@gmail.com>
2026-04-09 18:13:07 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.5 MB (92.67%)

--- leetcode/0050-powx-n/solution.cpp
+++ leetcode/0050-powx-n/solution.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-    double power(double x, long long n){
-        if(n==0) return 1.0;
-        if(n==1) return x;
-        if(n%2 ==0 ){
-            return power(x*x,n/2);
-        }
-        return x*power(x,n-1);
-    }
     double myPow(double x, int n) {
-        long long num=n;
-        if(num<0) {
-            return (1.0/power(x,-1*num));
+        double ans =1.0;
+        long long nn=n;
+        if(nn<0) nn=-1*nn;
+        while(nn){
+            if(nn&1) //odd
+            {
+                ans = ans*x;
+                nn=nn-1;
             }
-        return power(x,n);
+            else {
+                x=x*x; nn=nn/2;
+            }
+        }
+        if(n<0) ans = double (1.0)/double(ans);
+        return ans;
     }
 };
