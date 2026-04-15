aac3a5f026c18533e549ac415a5d4011eee6083a
Arushi Jain <arushij0607@gmail.com>
2026-04-15 16:59:18 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18.4 MB (37.57%)

---
+++ leetcode/2598-shortest-distance-to-target-string-in-a-circular-array/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int closestTarget(vector<string>& words, string target, int startIndex) {
+        if(words[startIndex]==target) return 0;
+        int n = words.size();
+        int i = 1;
+        int prev = (startIndex-i+n)%n;
+        int next = (startIndex+i+n)%n;
+        while(prev!=next){
+            if(words[prev] == target || words[next]==target) return i; 
+            i++;    
+            prev = (startIndex-i+n)%n;
+            next = (startIndex+i+n)%n;
+        }
+        if(words[prev] == target || words[next]==target) return i; 
+        return -1;
+    }
+};
