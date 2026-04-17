117cbca3f32e0c09e63c5af659f267666fd5fde9
Arushi Jain <arushij0607@gmail.com>
2026-04-17 07:55:02 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18.3 MB (66.58%)

--- leetcode/2598-shortest-distance-to-target-string-in-a-circular-array/solution.cpp
+++ leetcode/2598-shortest-distance-to-target-string-in-a-circular-array/solution.cpp
@@ -4,7 +4,7 @@ public:
         if(words[startIndex]==target) return 0;
         int n = words.size();
         int i = 1;
-        int prev = (startIndex-i+n)%n;
+        int prev = (startIndex-i+n)%n; //ADDING BY i TO KKNOW KITNE STEPS AAGE AA GYE 
         int next = (startIndex+i+n)%n;
         while(prev!=next){
             if(words[prev] == target || words[next]==target) return i; 
