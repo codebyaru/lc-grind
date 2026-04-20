51cd1d832d47a8faf7d485a4b51ea4becb3dceb0
Arushi Jain <arushij0607@gmail.com>
2026-04-20 15:16:46 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.2 MB (6.92%)

---
+++ leetcode/2199-two-furthest-houses-with-different-colors/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxDistance(vector<int>& colors) {
+        int n=colors.size();
+        for (int i=0;i<n;i++){
+            if(colors[i]^colors.back() | (colors[n-1-i]^colors[0]))
+            return n-1-i;
+        }
+        return 0;
+    }
+};
