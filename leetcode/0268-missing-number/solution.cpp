47a19b39cd4ca430f9759e6d66bdd23a3861918c
Arushi Jain <arushij0607@gmail.com>
2026-04-22 04:30:38 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 21.8 MB (37.98%)

--- leetcode/0268-missing-number/solution.cpp
+++ leetcode/0268-missing-number/solution.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        sort (nums.begin(),nums.end());
-        for (int i=0;i<nums.size();i++)
-        {
-            if (nums[i]!=i)
-            return i;
+        int n=nums.size();
+        int sum=(n*(n+1))/2; 
+        int s=0;
+        for(int i=0;i<nums.size();i++){
+            s+=nums[i];
         }
-        return nums.size();
+        return sum-s;
+       
     }
 };
