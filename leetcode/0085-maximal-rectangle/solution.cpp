965f56963c596955ec3fc0b8663054dcf86eba5a
Arushi Jain <arushij0607@gmail.com>
2026-05-01 16:27:44 +0000

    Sync LeetCode submission Runtime - 2 ms (93.05%), Memory - 20 MB (41.64%)

--- leetcode/0085-maximal-rectangle/solution.cpp
+++ leetcode/0085-maximal-rectangle/solution.cpp
@@ -1,41 +1,34 @@
 class Solution {
 public:
-    int area(vector<int>&heights){
-        int n= heights.size();
-        stack<int>s;
-        int maxiarea=0;
+    int calcarea(vector<int>histo){
+        int n = histo.size();
+        stack<int>st;
+        int maxi=0;
         for (int i=0;i<=n;i++){
-            int h=(i==n)?0:heights[i];
-            while(!s.empty()&&h<heights[s.top()]){
-                int height=heights[s.top()];
-                s.pop();
-                int width=s.empty()?i:i-s.top()-1;
-                maxiarea=max(maxiarea,height*width);
+            while(!st.empty() && (i==n || histo[st.top()]>=histo[i])){
+                int h = histo[st.top()];
+                st.pop();
+                int width ;
+                if (st.empty()) width =i;
+                else width = i-1-st.top();
+                maxi = max(maxi, h*width);
             }
-            s.push(i);
+            st.push(i);
         }
-        return maxiarea;
+        return maxi;
     }
     int maximalRectangle(vector<vector<char>>& matrix) {
-        int rows=matrix.size();
-        if(rows==0) return 0;
-        int cols = matrix[0].size();
-        vector<vector<int>>height(rows,vector<int>(cols,0));
-        for (int i=0;i<rows;i++){
-            for (int j=0;j<cols;++j){
-                if(matrix[i][j]=='1'){
-                    height[i][j]=(i==0?0:height[i-1][j])+1;
-                }
-                else{
-                    height[i][j]=0;
-                }
-                }
+        int row = matrix.size(); int col = matrix[0].size();
+        int maxa=0;
+        vector<int>h(col,0);
+        for (int i=0;i<row;i++){
+            for (int j=0;j<col;j++){
+                if(matrix[i][j]=='1') h[j]++; //char vector given LOL :)
+                else h[j]=0;
             }
-            int maxiarea=0;
-            for (int i=0;i<rows;i++){
-                maxiarea=max(maxiarea,area(height[i]));
-            }
-            return maxiarea;
-        
+            int area = calcarea(h);
+            maxa=max(maxa,area);
+        }
+        return maxa;
     }
 };
