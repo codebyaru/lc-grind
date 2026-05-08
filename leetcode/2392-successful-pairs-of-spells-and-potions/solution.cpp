646d337f6ef8be9e12e05220d39cfa63ebb90a04
Arushi Jain <arushij0607@gmail.com>
2026-05-08 13:02:59 +0000

    Sync LeetCode submission Runtime - 39 ms (86.75%), Memory - 128.4 MB (9.71%)

---
+++ leetcode/2392-successful-pairs-of-spells-and-potions/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        int n=spells.size(); int m = potions.size();
+        vector<int>ans;
+        sort(potions.begin(), potions.end());
+        for(int i=0;i<n;i++){
+            int low = 0; int high = m-1;
+            while(low<=high){
+                int mid = (low+high)/2;
+                if((long long)spells[i] * potions[mid] >= success)
+                high = mid-1;
+                else low = mid+1;
+            }
+            ans.push_back(m-low);
+        }
+        return ans;
+    }
+};
