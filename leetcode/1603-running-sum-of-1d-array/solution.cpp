33e4dd8415abd7874e7cffc5feaec84b728ff7d7
Arushi Jain <arushij0607@gmail.com>
2026-05-08 16:04:54 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.5 MB (51.02%)

--- leetcode/1603-running-sum-of-1d-array/solution.cpp
+++ leetcode/1603-running-sum-of-1d-array/solution.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
-        vector<int> sum;
-        int ssum = 0;
-        for (int i = 0; i < nums.size(); i++) {
-
-            ssum = ssum + nums[i];
-            sum.push_back(ssum);
+        for(int i=1;i<nums.size();i++){
+            nums[i] = nums[i] + nums[i-1];
         }
-        return sum;
+        return nums;
     }
 };
