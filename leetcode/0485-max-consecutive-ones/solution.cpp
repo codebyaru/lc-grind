73e9f3428c2e61b50bc28eead04bd23d2d50062b
Arushi Jain <arushij0607@gmail.com>
2026-05-08 20:39:21 +0000

    Sync LeetCode submission Runtime - 3 ms (18.80%), Memory - 50.3 MB (32.34%)

--- leetcode/0485-max-consecutive-ones/solution.cpp
+++ leetcode/0485-max-consecutive-ones/solution.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
-        int l = 0, r = 0, ans = 0;
-        while (r < nums.size()) {
-            if (nums[r] == 1)
-                ans = max(ans, r - l + 1);
-            else
-                l = r + 1;
-            r++;
+        int c=0; int ans =0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==1) {
+                c++;
+                 ans=max(ans,c);}
+                else c=0;
         }
         return ans;
     }
