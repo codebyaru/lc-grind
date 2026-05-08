c494fbe2e4a3e0c6c1ac78e0edf8558e72a29b59
Arushi Jain <arushij0607@gmail.com>
2026-05-08 16:46:17 +0000

    Sync LeetCode submission Runtime - 349 ms (13.05%), Memory - 228.2 MB (15.84%)

--- leetcode/2270-find-all-lonely-numbers-in-the-array/solution.cpp
+++ leetcode/2270-find-all-lonely-numbers-in-the-array/solution.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     vector<int> findLonely(vector<int>& nums) {
-        vector<int> ans;
-        unordered_map<int, int> lonely;
-        for (auto it : nums)
-            lonely[it]++;
-        for (pair<int, int> it : lonely) {
-            int x = it.first;
-            int y = it.second;
-            if (y == 1 && lonely.count(x + 1) == 0 && lonely.count(x - 1) == 0)
-                ans.push_back(x);
+        unordered_map<int,int>mp;
+        vector<int>ans;
+        for(int x:nums) mp[x]++;
+        for(int i=0;i<nums.size();i++){
+            int temp = nums[i];
+            if(mp[temp]==1&&mp[temp+1]==0&&mp[temp-1]==0) ans.push_back(temp);
+            //mp[temp] is checking it.second anf tmep is it.first
         }
         return ans;
     }
