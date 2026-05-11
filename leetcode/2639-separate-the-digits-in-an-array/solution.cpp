16f68927edc6872db004a2452d5486cddc7d3156
Arushi Jain <arushij0607@gmail.com>
2026-05-11 09:24:33 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 15.1 MB (75.92%)

---
+++ leetcode/2639-separate-the-digits-in-an-array/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> separateDigits(vector<int>& nums) {
+        vector<int>v;
+        for(int i=0;i<nums.size();i++){
+            string s=to_string(nums[i]);
+            for(int j=0;j<s.size();j++){
+                v.push_back(s[j]-'0');
+            }
+        }
+        return v;
+    }
+};
