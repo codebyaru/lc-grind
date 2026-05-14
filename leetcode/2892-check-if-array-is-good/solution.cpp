b9f80e6b9b3180210793068dbcb30761b1f87d39
Arushi Jain <arushij0607@gmail.com>
2026-05-14 14:54:40 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 27.2 MB (37.11%)

--- leetcode/2892-check-if-array-is-good/solution.cpp
+++ leetcode/2892-check-if-array-is-good/solution.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     bool isGood(vector<int>& nums) {
-        int n = *max_element(nums.begin(), nums.end());
-        if(nums.size() != n + 1) return false;
-
-        unordered_map<int,int> freq;
-        for(int i = 0; i < nums.size(); i++){
-            freq[nums[i]]++;
+        int n = nums.size()-1;
+        if(n<1) return false;
+        
+        vector<int>freq(n+1,0);
+        for(int x:nums){
+            if(x<1 || x>n) return false;
+            freq[x]++;
         }
-        for(int i = 1; i < n; i++){
-            if(freq[i] != 1) return false;
+        for(int i=1;i<n;i++){
+            if(freq[i]!=1) return false;
         }
-        if(freq[n] != 2) return false;
-        return true;
+        return freq[n]==2;
     }
 };
