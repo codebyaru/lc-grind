166869f47a501aae6ef9d4494371fd4aae07c217
Arushi Jain <arushij0607@gmail.com>
2026-05-17 06:18:45 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 26.1 MB (67.55%)

--- leetcode/0540-single-element-in-a-sorted-array/solution.cpp
+++ leetcode/0540-single-element-in-a-sorted-array/solution.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int n = nums.size();
-        int low = 1;
-        int high = n - 2;
-        if (n == 1)
-            return nums[0];
-        if (nums[0] != nums[1])
-            return nums[0];
-        if (nums[n - 1] != nums[n - 2])
-            return nums[n - 1];
-        while (low <=high) {
+        int low = 0, high = nums.size() - 1;
+
+        while(low < high) {
             int mid = (low + high) / 2;
-            if (nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1])
-                return nums[mid];
-            if ((mid % 2 == 1 && nums[mid] == nums[mid - 1]) ||
-                (mid % 2 == 0 && nums[mid] == nums[mid + 1]))
-                low = mid + 1;
+
+            if(mid % 2 == 1) mid--; //mid is even, at odd index
+
+            if(nums[mid] == nums[mid + 1])
+                low = mid + 2;
             else
-                high = mid - 1;
+                high = mid;
         }
-        return -1;
+
+        return nums[low];
     }
 };
